6bf709cfcbe862cca21149a36702bbdc267c7223
Tom Nguyen <tom81094@gmail.com>
2022-06-30 16:49:41 +0700

    chore: add sample import and use on main
    We have all our examples in our test files, so this is just a filler
    commit.

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	t "github.com/monotykamary/nested-modal-transducers-golang-implementation/transducer"
+)
+
+func main() {
+	config, lightTransducer := t.NewLightMachine(t.PedestrianRed.String(), t.Stop.String())
+	outputs := lightTransducer.Transduce(config, t.PedestrianTimer)
+
+	nextState := outputs.GetState()
+	nextChildState := outputs.GetChildState(t.PedestrianLightTransducerName)
+
+	fmt.Println(nextState.String(), nextChildState.String())
 }
